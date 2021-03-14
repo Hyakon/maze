aa1547681388ccffa7fecea10e304dcc89f787a1
Paul K <azn.baoul@gmail.com>
2021-03-14 20:18:02 +0100

    clean some code

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
 const table = document.querySelector("table");
 
-const [height, width] = [5, 10];
-console.log(table, height, width);
-
-generateMaze = () => {
-  const board = generateArray(height, width);
-  return board.map((row) => row.map((cell) => generateValue()));
-};
 const dec2bin = (dec) => {
   return (dec >>> 0).toString(2);
-};
-
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * Math.floor(max)) + 1;
-};
-
-const generateValue = () => {
-  return dec2bin(getRandomInt(14)).padStart(4, "0");
 };
 
 const sample = (array) => {
   return array[Math.floor(Math.random() * array.length)];
 };
-
-const goNext = () => {};
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
 const UNVISITED = "1111";
 const LEFT = "1110";
@@ -31,17 +17,6 @@ const RIGHT = "1011";
 const TOP = "0111";
 const BOTTOM = "1101";
 
-function removeItemOnce(arr, value) {
-  var index = arr.indexOf(value);
-  if (index > -1) {
-    arr.splice(index, 1);
-  }
-  return arr;
-}
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
 class Maze {
   constructor(width, height) {
     this.width = width;
@@ -56,43 +31,55 @@ class Maze {
 
   initMaze = () => {
     this.generateMaze();
-    console.log(this.maze);
-    // this.searchNextCell({ x: 0, y: 0 });
-    let a = this.solveMaze({ x: 0, y: 0 });
-    console.log(this.last);
+
+    const startingPos = this.initPos();
+
+    this.openMaze(startingPos);
+    this.solveMaze(startingPos);
     this.finishMaze();
     this.displayMaze();
   };
 
+  initPos = () => {
+    const pos = { x: getRandomInt(this.height), y: 0 };
+    return pos;
+  };
+
   finishMaze = () => {
-    const { x, y } = this.last;
-    console.log(this.last, x, y, this.height, this.width);
+    this.openMaze(this.last);
+  };
+
+  openMaze = ({ x, y }) => {
     let value = parseInt(this.maze[y][x], 2);
-    if (this.last.x === 0)
+
+    if (x === 0)
       return (this.maze[y][x] = dec2bin(value & parseInt(LEFT, 2)).padStart(
         4,
         "0"
       ));
-    if (this.last.y === 0)
+    if (y === 0)
       return (this.maze[y][x] = dec2bin(value & parseInt(TOP, 2)).padStart(
         4,
         "0"
       ));
-    if (this.last.x === this.width - 1) {
+    if (x === this.width - 1) {
       console.log("right", x, y);
       return (this.maze[y][x] = dec2bin(value & parseInt(RIGHT, 2)).padStart(
         4,
         "0"
       ));
     }
-    if (this.last.y === this.height - 1) {
+    if (y === this.height - 1) {
       console.log("bottom", x, y);
       return (this.maze[y][x] = dec2bin(value & parseInt(BOTTOM, 2)).padStart(
         4,
         "0"
       ));
     }
-    // this.maze[y][x] = "";
+  };
+
+  openCell = ({ x, y }, direction) => {
+    let value = parseInt(this.maze[y][x], 2);
   };
 
   checkLeft = ({ x, y }) => {
@@ -118,90 +105,48 @@ class Maze {
     if (this.maze[y + 1][x] === UNVISITED) return true;
     return false;
   };
-  searchNextCell = (pos) => {
-    // console.log("searching next", pos);
-    const { x, y } = pos;
-    let nexts = [];
 
-    if (this.checkLeft(pos))
-      nexts.push({ x: x - 1, y, direction: LEFT, previous: RIGHT });
-    if (this.checkRight(pos))
-      nexts.push({ x: x + 1, y, direction: RIGHT, previous: LEFT });
-    if (this.checkTop(pos))
-      nexts.push({ x, y: y - 1, direction: TOP, previous: BOTTOM });
-    if (this.checkBottom(pos))
-      nexts.push({ x, y: y + 1, direction: BOTTOM, previous: TOP });
-    // console.log(nexts);
-    // this.nexts = nexts;
-    // while (nexts.length) {
-    // console.log("in while", nexts);
-    // const next = sample(nexts);
-    // nexts = removeItemOnce(nexts, next);
-    // if (next) {
-    //   this.moveCell(next, pos);
-    //   this.searchNextCell({ x: next.x, y: next.y });
-    //   console.log("b");
-    // } else return false;
-    // console.log(nexts);
-    // }
-    // console.log("ok", this.nexts);
-    return nexts;
+  searchNextCell = (currentPos) => {
+    const { x, y } = currentPos;
+    const nextCells = [];
+
+    if (this.checkLeft(currentPos))
+      nextCells.push({ x: x - 1, y, direction: LEFT, previous: RIGHT });
+    if (this.checkRight(currentPos))
+      nextCells.push({ x: x + 1, y, direction: RIGHT, previous: LEFT });
+    if (this.checkTop(currentPos))
+      nextCells.push({ x, y: y - 1, direction: TOP, previous: BOTTOM });
+    if (this.checkBottom(currentPos))
+      nextCells.push({ x, y: y + 1, direction: BOTTOM, previous: TOP });
+    return nextCells;
   };
 
-  solveMaze = (pos) => {
-    console.log("h");
-    let nexts = this.searchNextCell(pos);
-    console.log(nexts);
-    while (nexts.length) {
-      const next = sample(nexts);
-      this.moveCell(next, pos);
+  solveMaze = (currentPos) => {
+    let nextCells = this.searchNextCell(currentPos);
+    while (nextCells.length) {
+      const next = sample(nextCells);
+      this.moveNextCell(next, currentPos);
       this.last = next;
       this.solveMaze({ x: next.x, y: next.y });
-      nexts = this.searchNextCell({ x: pos.x, y: pos.y });
+      nextCells = this.searchNextCell({ x: currentPos.x, y: currentPos.y });
     }
-    return [pos, nexts];
+    return [currentPos, nextCells];
   };
 
-  moveCell = (nextPos, { x, y }) => {
-    nextPos.direction;
-    console.log(x, y, nextPos.direction, this.maze[y][x]);
+  moveNextCell = (nextCell, { x, y }) => {
+    const currentValue = parseInt(this.maze[y][x], 2);
+    const nextValue = parseInt(this.maze[nextCell.y][nextCell.x], 2);
+    const previousDirection = parseInt(nextCell.previous, 2);
+    const nextDirection = parseInt(nextCell.direction, 2);
 
-    let value = parseInt(this.maze[y][x], 2);
-    let valueNext = parseInt(this.maze[nextPos.y][nextPos.x], 2);
-    let prev = parseInt(nextPos.previous, 2);
-    let next = parseInt(nextPos.direction, 2);
-    // console.log("dec", value, prev, next, value & prev & next);
-    // console.log(
-    //   "binary",
-    //   dec2bin(value),
-    //   dec2bin(prev),
-    //   dec2bin(next),
-    //   dec2bin(value & prev & next).padStart(4, "0")
-    // );
-    this.maze[y][x] = dec2bin(value & next).padStart(4, "0");
-    this.maze[nextPos.y][nextPos.x] = dec2bin(valueNext & prev).padStart(
-      4,
-      "0"
-    );
-    this.displayMaze();
+    this.maze[y][x] = dec2bin(currentValue & nextDirection).padStart(4, "0");
+    this.maze[nextCell.y][nextCell.x] = dec2bin(
+      nextValue & previousDirection
+    ).padStart(4, "0");
   };
 
-  createStyledCell = (value) => {
-    const td = document.createElement("td");
-    td.style.height = "50px";
-    td.style.width = "50px";
-    if (value !== UNVISITED) td.style.background = "red";
-    if (value[0] === "1") td.style.borderTop = "5px solid black";
-    if (value[1] === "1") td.style.borderRight = "5px solid black";
-    if (value[2] === "1") td.style.borderBottom = "5px solid black";
-    if (value[3] === "1") td.style.borderLeft = "5px solid black";
-    return td;
-  };
-
-  displayMaze = async () => {
-    await sleep(1000);
-
-    document.querySelector("table").remove();
+  displayMaze = () => {
+    document.querySelector("table")?.remove();
     const table = document.createElement("table");
     this.maze.forEach((row, y) => {
       const tr = document.createElement("tr");
@@ -213,15 +158,17 @@ class Maze {
     });
     document.querySelector("body").append(table);
   };
+
+  createStyledCell = (value) => {
+    const td = document.createElement("td");
+    const size = `${500 / Math.max(this.height, this.width)}px`;
+    td.style.height = size;
+    td.style.width = size;
+    if (value !== UNVISITED) td.style.background = "red";
+    if (value[0] === "1") td.style.borderTop = "5px solid black";
+    if (value[1] === "1") td.style.borderRight = "5px solid black";
+    if (value[2] === "1") td.style.borderBottom = "5px solid black";
+    if (value[3] === "1") td.style.borderLeft = "5px solid black";
+    return td;
+  };
 }
-
-// const start = () => {
-//   let board = initMaze();
-
-//   const pos = { x: 2, y: 2 };
-//   nexts = searchNext(board, pos);
-//   goNext(board, nexts);
-//   displayMaze(board);
-// };
-
-// start();
